b28cd4e03f1ceeda32eb42cfad2c6d5a1ec7f74e
Lee Edwards <leeredwards@gmail.com>
2011-02-11 22:37:50 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swccgonline::Application.config.secret_token = '3443de4d6a3dc02ee49bcb6c2569d4b3c132c7b62a9bd61f08f95edcf122c91c3e9650cf1263631b7ca420f84a17f7f8506eba74cd3eb8f66521e257abff97d8'
