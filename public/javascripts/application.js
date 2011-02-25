b126feb35a5e71978bb3678124445dff382b461f
Lee Edwards <leeredwards@gmail.com>
2011-02-24 23:30:55 -0500

    Text hinting in Javascript

---
+++ public/javascripts/application.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $("#user_email").tbHinter({text: "E-MAIL"});
+  $("#user_password").tbHinter({text: "password"});
+  $("#user_password_confirmation").tbHinter({text: "password"});
+  $("#search_field").tbHinter({text: "Search for cards"});
+});
